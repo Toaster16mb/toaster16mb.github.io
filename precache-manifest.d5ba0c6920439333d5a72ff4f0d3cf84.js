cc733affb3e03812a921e38a9f1b08a4468c4d20
s.alekseev <s.alekseev@hypeauditor.com>
2022-06-26 11:06:22 +0300

    init

---
+++ precache-manifest.d5ba0c6920439333d5a72ff4f0d3cf84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "10dafe598361e7febb50",
+    "url": "/static/js/main.10dafe59.chunk.js"
+  },
+  {
+    "revision": "238c47083c669806a3cb",
+    "url": "/static/js/1.238c4708.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/static/media/walletconnect-banner.e1cb8d21.png"
+  },
+  {
+    "revision": "906078994f7221a25b900f47425b3ecc",
+    "url": "/static/media/erc20.90607899.svg"
+  },
+  {
+    "revision": "42bca7317690c3f5e9ecd8db387344df",
+    "url": "/static/media/eth.42bca731.svg"
+  },
+  {
+    "revision": "e37299a879ceb5a23000d8a2bb10bc46",
+    "url": "/static/media/xdai.e37299a8.png"
+  },
+  {
+    "revision": "70a229155fd1bbae18bd4e72b0da2147",
+    "url": "/index.html"
+  }
+];
